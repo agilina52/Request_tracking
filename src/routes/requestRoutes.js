69f475d15b2cd3077e3215f62cc00b815f625a94
agilina52 <agilina52@yandex.ru>
2026-09-20 22:39:24 +0300

    feat: wrap route handlers with async error handler

--- src/routes/requestRoutes.js
+++ src/routes/requestRoutes.js
@@ -1,18 +1,19 @@
 'use strict';
 
 const { Router } = require('express');
+const asyncHandler = require('../middlewares/asyncHandler');
 const validate = require('../middlewares/validate');
 const requestSchemas = require('../validators/requestSchemas');
 
 function createRequestRouter(controller) {
   const router = Router();
 
-  router.get('/', validate({ query: requestSchemas.listQuery }), controller.list);
-  router.post('/', validate({ body: requestSchemas.createBody }), controller.create);
-  router.get('/:id', validate({ params: requestSchemas.idParams }), controller.getById);
-  router.patch('/:id', validate({ params: requestSchemas.idParams, body: requestSchemas.updateBody }), controller.update);
-  router.patch('/:id/status', validate({ params: requestSchemas.idParams, body: requestSchemas.statusBody }), controller.changeStatus);
-  router.delete('/:id', validate({ params: requestSchemas.idParams }), controller.delete);
+  router.get('/', validate({ query: requestSchemas.listQuery }), asyncHandler(controller.list));
+  router.post('/', validate({ body: requestSchemas.createBody }), asyncHandler(controller.create));
+  router.get('/:id', validate({ params: requestSchemas.idParams }), asyncHandler(controller.getById));
+  router.patch('/:id', validate({ params: requestSchemas.idParams, body: requestSchemas.updateBody }), asyncHandler(controller.update));
+  router.patch('/:id/status', validate({ params: requestSchemas.idParams, body: requestSchemas.statusBody }), asyncHandler(controller.changeStatus));
+  router.delete('/:id', validate({ params: requestSchemas.idParams }), asyncHandler(controller.delete));
 
   return router;
 }
