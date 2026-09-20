f4afd871af97cc93d2a75e206b8ed3bb388a2d8f
agilina52 <agilina52@yandex.ru>
2026-09-20 22:41:47 +0300

    feat: protect request mutating routes with api key

--- src/routes/requestRoutes.js
+++ src/routes/requestRoutes.js
@@ -2,6 +2,7 @@
 
 const { Router } = require('express');
 const asyncHandler = require('../middlewares/asyncHandler');
+const apiKeyAuth = require('../middlewares/apiKeyAuth');
 const validate = require('../middlewares/validate');
 const requestSchemas = require('../validators/requestSchemas');
 
@@ -9,11 +10,11 @@ function createRequestRouter(controller) {
   const router = Router();
 
   router.get('/', validate({ query: requestSchemas.listQuery }), asyncHandler(controller.list));
-  router.post('/', validate({ body: requestSchemas.createBody }), asyncHandler(controller.create));
+  router.post('/', apiKeyAuth, validate({ body: requestSchemas.createBody }), asyncHandler(controller.create));
   router.get('/:id', validate({ params: requestSchemas.idParams }), asyncHandler(controller.getById));
-  router.patch('/:id', validate({ params: requestSchemas.idParams, body: requestSchemas.updateBody }), asyncHandler(controller.update));
-  router.patch('/:id/status', validate({ params: requestSchemas.idParams, body: requestSchemas.statusBody }), asyncHandler(controller.changeStatus));
-  router.delete('/:id', validate({ params: requestSchemas.idParams }), asyncHandler(controller.delete));
+  router.patch('/:id', apiKeyAuth, validate({ params: requestSchemas.idParams, body: requestSchemas.updateBody }), asyncHandler(controller.update));
+  router.patch('/:id/status', apiKeyAuth, validate({ params: requestSchemas.idParams, body: requestSchemas.statusBody }), asyncHandler(controller.changeStatus));
+  router.delete('/:id', apiKeyAuth, validate({ params: requestSchemas.idParams }), asyncHandler(controller.delete));
 
   return router;
 }
