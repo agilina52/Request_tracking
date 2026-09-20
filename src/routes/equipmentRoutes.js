774a3e78ec093b6f495c76ed2afa80712153d418
agilina52 <agilina52@yandex.ru>
2026-09-20 22:41:38 +0300

    feat: protect equipment mutating routes with api key

--- src/routes/equipmentRoutes.js
+++ src/routes/equipmentRoutes.js
@@ -2,6 +2,7 @@
 
 const { Router } = require('express');
 const asyncHandler = require('../middlewares/asyncHandler');
+const apiKeyAuth = require('../middlewares/apiKeyAuth');
 const validate = require('../middlewares/validate');
 const equipmentSchemas = require('../validators/equipmentSchemas');
 const requestSchemas = require('../validators/requestSchemas');
@@ -10,12 +11,12 @@ function createEquipmentRouter(controller) {
   const router = Router();
 
   router.get('/', validate({ query: equipmentSchemas.listQuery }), asyncHandler(controller.list));
-  router.post('/', validate({ body: equipmentSchemas.createBody }), asyncHandler(controller.create));
+  router.post('/', apiKeyAuth, validate({ body: equipmentSchemas.createBody }), asyncHandler(controller.create));
   router.get('/:id/requests', validate({ params: equipmentSchemas.idParams, query: requestSchemas.listQuery }), asyncHandler(controller.listRequests));
   router.get('/:id/weather', validate({ params: equipmentSchemas.idParams }), asyncHandler(controller.getWeather));
   router.get('/:id', validate({ params: equipmentSchemas.idParams }), asyncHandler(controller.getById));
-  router.patch('/:id', validate({ params: equipmentSchemas.idParams, body: equipmentSchemas.updateBody }), asyncHandler(controller.update));
-  router.delete('/:id', validate({ params: equipmentSchemas.idParams }), asyncHandler(controller.delete));
+  router.patch('/:id', apiKeyAuth, validate({ params: equipmentSchemas.idParams, body: equipmentSchemas.updateBody }), asyncHandler(controller.update));
+  router.delete('/:id', apiKeyAuth, validate({ params: equipmentSchemas.idParams }), asyncHandler(controller.delete));
 
   return router;
 }
